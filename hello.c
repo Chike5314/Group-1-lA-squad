835334d3669cc5dd64a1fd4233ff634a066a4d34
chike5314 <chikechewakondi@gmail.com>
2024-10-31 18:10:15 +0100

    added another string

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 
- int main(){
-printf("Hello");
-
+int main(){
+   printf("Hello");
+    //chike's contribution to this repo that is most important
+    printf("The most important aspect of this project\n");
  }
